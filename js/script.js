7df31b7069baa9e16725e287b34187e643c66286
younus-always <younusofficial443@gmail.com>
2024-07-10 22:05:13 +0600

    IT solution plartform

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+// Shopping cart-Popup
+
+let Shopping = addEventListener(click, ".shoppingcart");
+const Popup = document.querySelector(".shoppingcart-popup");
+let toggle = document.querySelector(".show");
+
+Shopping () => {
+if(Shopping.click === (".shoppingcart-popup")){
+  document.querySelector(".shoppingcart-popup").add.classList(".show");
+}else{
+  document.querySelector(".shoppingcart-popup").remove.classList("show");
+}}
+Shopping();
