ae768114f474b84dc98bad31e5a7dab3dab2b730
younus-always <younusofficial443@gmail.com>
2025-03-01 00:29:59 +0600

    changes something

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
+// Cart open function
 
-// Shopping cart-Popup
+let open = document.querySelector('.shoppingcart-popup').style.display = "none";
 
-let Shopping = addEventListener(click, ".shoppingcart");
-const Popup = document.querySelector(".shoppingcart-popup");
-let toggle = document.querySelector(".show");
+document.querySelector(".shoppingcart").addEventListener('click', () => {
+  document.querySelector('.shoppingcart-popup').style.display = "block";
+});
 
-Shopping () => {
-if(Shopping.click === (".shoppingcart-popup")){
-  document.querySelector(".shoppingcart-popup").add.classList(".show");
-}else{
-  document.querySelector(".shoppingcart-popup").remove.classList("show");
-}}
-Shopping();
+
